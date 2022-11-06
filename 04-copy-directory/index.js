0e37d249709e5a2eda91d43f4a2d40280c24f48f
Anubic29 <asitailo2039@gmail.com>
2022-11-06 21:43:27 +0200

    complete 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.rm(path.resolve(__dirname, 'files-copy'), { recursive: true }, generateFolder)
+
+function generateFolder() {
+  fs.mkdir(path.resolve(__dirname, 'files-copy'), fillFolder)
+}
+
+function fillFolder() {
+  fs.readdir(path.resolve(__dirname, 'files'), (err, data) => {
+    data.forEach(file => {
+      fs.copyFile(path.resolve(__dirname, `files\\${file}`), path.resolve(__dirname, `files-copy\\${file}`), () => {})
+    })
+  })
+}
